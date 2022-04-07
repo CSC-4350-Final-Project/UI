d029fa6b92b2c900d9904b9f7ff3e2276cf3ab58
Facundo Figueroa <facundof13@gmail.com>
2022-04-07 18:10:42 -0400

    add search/search results page

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,65 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Row,
+} from 'react-bootstrap';
+import SearchResults from './SearchResults/SearchResults';
+import event from '../Event/dummy-event';
+
+const dummyResults = [event, event, event];
 
 function Search() {
+  const [form, setForm] = useState({ keyword: '', postalCode: '' });
+  const [searchResults, setSearchResults] = useState([]);
+  const [searched, setSearched] = useState(false);
+
+  async function search() {
+    // const body = JSON.stringify(form);
+    setSearchResults(dummyResults);
+    setSearched(true);
+  }
+
+  function clear() {
+    setSearchResults([]);
+    setSearched(false);
+  }
+
   return (
-    <div>
-      Search Component
-    </div>
+    <Container>
+      <Form onSubmit={(e) => { e.preventDefault(); search(); }}>
+        <Row className="mt-3 bg-light rounded p-2 shadow-sm">
+          <Row>
+            <Col>
+              <h4 className="text-center">Search for an Event</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={8}>
+              <Form.Group className="m-2">
+                <Form.Label>Keyword</Form.Label>
+                <Form.Control type="text" value={form.keyword} onChange={(e) => setForm({ ...form, keyword: e.target.value })} />
+              </Form.Group>
+            </Col>
+            <Col xs={12} sm={4}>
+              <Form.Group className="m-2">
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control type="text" value={form.postalCode} onChange={(e) => setForm({ ...form, postalCode: e.target.value })} />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center mt-3" xs={12}>
+              <Button onClick={() => search()} variant="outline-primary">Submit</Button>
+              <Button onClick={() => clear()} className="ms-2" variant="outline-secondary">Clear</Button>
+            </Col>
+          </Row>
+        </Row>
+      </Form>
+      {searched && <SearchResults results={searchResults} />}
+    </Container>
   );
 }
 
